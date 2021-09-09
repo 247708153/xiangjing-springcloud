1349667e0984759ed41ab3c9c5d94431acf3c46e
xiangjing <247708153@qq.com>
2021-09-09 08:41:19 +0800

    feat(修改sh脚本): 修改docker-compose 修改env里面的配置

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo '===========1.preparing for development Environment==============='
 echo '1.1:make sure java8,git and maven are installed and set up environment variables'
 echo '1.2:make sure docker and docker-compose are installed and environment variables are set up'
 
-#确认环境信息准备就绪
+#verify that the environment variables are ready
 read -r -p "is the development environment ready? [Y/n] " envConfirm
 case $envConfirm in
     [yY][eE][sS]|[yY])
@@ -18,3 +18,27 @@ case $envConfirm in
 		exit 1
 		;;
 esac
+
+echo '==================1.3 optionally clean up containers and images that are currently started by ths script=============='
+#optionally clean up containers and images that are currently started by ths script
+docker stop sc-rabbitmq sc-redis sc-mysql
+docker rm sc-rabbitmq sc-redis sc-mysql
+
+#docker stop sc-monitor-admin sc-authorization-server sc-authentication-server sc-organization sc-gateway-admin sc-gateway-web
+#docker rm sc-monitor-admin sc-authorization-server sc-authentication-server sc-organization sc-gateway-admin sc-gateway-web
+#docker image rm cike/admin cike/authorization-server:latest cike/authentication-server:latest cike/organization:latest cike/gateway-admin:latest cike/gateway-web:latest
+
+
+echo '==================4.docker-compose launch of public services==================='
+#去docker-compose目录
+cd docker-compose
+echo '==================4.1display environment variables: docker-compose/.env =========='
+#显示环境变量
+cat ./.env
+echo ''
+
+#按需要开启公共服务
+#echo '==================4.2启动 mysql or redis or rabbitmq ========'
+docker-compose -f docker-compose.yml up -d mysql
+docker-compose -f docker-compose.yml up -d redis
+docker-compose -f docker-compose.yml up -d rabbitmq
